3cf94cfd065a6d5a8a3a40c42e46377ec85652c1
Giulia <giuvenct@gmail.com>
2020-06-29 14:17:20 +0200

    Add files via upload

---
+++ BulbaCentral.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Pokémon Central Redirecter
+// @version      alpha
+// @description  redirects pokémon central to bulbapedia
+// @author       Julia
+// @include      https://wiki.pokemoncentral.it/*
+// @grant        none
+// ==/UserScript==
+
+//window.open
+location.href = (document.getElementsByClassName("interlanguage-link-target", "_self")[1]);
+
+
+
+
+
